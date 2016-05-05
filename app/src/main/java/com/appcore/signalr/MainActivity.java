0b884859accfa3bc9f744b88abc7554fdac41e85
Jeremy Woo <jeremy@sort-it.co.nz>
2016-05-06 11:06:33 +1200

    added client test code.

--- app/src/main/java/com/appcore/signalr/MainActivity.java
+++ app/src/main/java/com/appcore/signalr/MainActivity.java
@@ -2,9 +2,15 @@ package com.appcore.signalr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import java.util.concurrent.ExecutionException;
 
 import microsoft.aspnet.signalr.client.Platform;
+import microsoft.aspnet.signalr.client.SignalRFuture;
 import microsoft.aspnet.signalr.client.http.android.AndroidPlatformComponent;
+import microsoft.aspnet.signalr.client.hubs.HubConnection;
+import microsoft.aspnet.signalr.client.hubs.HubProxy;
+import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +18,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Platform.loadPlatformComponent(new AndroidPlatformComponent());
+
+        HubConnection connection = new HubConnection("http://localhost/apptest/hub");
+        HubProxy proxy = connection.createHubProxy("mydemohub");
+        SignalRFuture<Void> awaitConnection = connection.start();
+
+        try {
+            awaitConnection.get();
+        }
+        catch (InterruptedException interruptException)
+        {
+            Log.v("", interruptException.getLocalizedMessage());
+        }
+        catch (ExecutionException executionException)
+        {
+            Log.v("", executionException.getLocalizedMessage());
+        }
+
+        proxy.invoke("JoinGroup", "Group1");
+        proxy.on("livefeed", new SubscriptionHandler1<String>() {
+            @Override
+            public void run(String s) {
+                Log.d("", s);
+            }
+        }, String.class);
     }
 }
